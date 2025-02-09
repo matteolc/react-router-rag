ef2a3600e31ad2f711ef5c82615d665cb34ac900
Matteo La Cognata <matteo.lacognata@gmail.com>
2025-02-09 21:38:34 +0100

    chore(): maybe fix single fetch

--- vite.config.ts
+++ vite.config.ts
@@ -3,35 +3,38 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import dotenvExpand from "dotenv-expand";
+import { installGlobals } from "@remix-run/node";
 
 export default defineConfig(({ isSsrBuild, command, mode }) => {
-  let define: Record<string, string> = {};
-  if (mode === "development") {
-    const env = loadEnv(mode, process.cwd(), "");
-    dotenvExpand.expand({ parsed: env });
-    define = {
-      "process.env": JSON.stringify(env),
-    };
-  } else {
-    define = {
-      "process.env.BLOB_READ_WRITE_TOKEN": JSON.stringify(
-        process.env.BLOB_READ_WRITE_TOKEN,
-      ),
-      "process.env.SUPABASE_URL": JSON.stringify(process.env.SUPABASE_URL),
-      "process.env.SUPABASE_ANON_KEY": JSON.stringify(
-        process.env.SUPABASE_ANON_KEY,
-      ),
-    };
-  }
+	let define: Record<string, string> = {};
+	if (mode === "development") {
+		const env = loadEnv(mode, process.cwd(), "");
+		dotenvExpand.expand({ parsed: env });
+		define = {
+			"process.env": JSON.stringify(env),
+		};
+	} else {
+		define = {
+			"process.env.BLOB_READ_WRITE_TOKEN": JSON.stringify(
+				process.env.BLOB_READ_WRITE_TOKEN,
+			),
+			"process.env.SUPABASE_URL": JSON.stringify(process.env.SUPABASE_URL),
+			"process.env.SUPABASE_ANON_KEY": JSON.stringify(
+				process.env.SUPABASE_ANON_KEY,
+			),
+		};
+	}
 
-  return {
-    define,
-    ssr: {
-      noExternal: command === "build" ? true : undefined,
-    },
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-    server: {
-      allowedHosts: true,
-    },
-  };
+	installGlobals({ nativeFetch: true });
+
+	return {
+		define,
+		ssr: {
+			noExternal: command === "build" ? true : undefined,
+		},
+		plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+		server: {
+			allowedHosts: true,
+		},
+	};
 });
