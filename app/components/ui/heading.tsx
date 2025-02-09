b202464df99ae153e2c262de20a53bd3ccd6e187
Matteo La Cognata <matteo.lacognata@gmail.com>
2025-02-09 23:31:07 +0100

    chore(): cosmetic

--- app/components/ui/heading.tsx
+++ app/components/ui/heading.tsx
@@ -1,60 +1,60 @@
 import clsx from "clsx";
 
 type HeadingProps = {
-  level?: 1 | 2 | 3 | 4 | 5 | 6;
+	level?: 1 | 2 | 3 | 4 | 5 | 6;
 } & React.ComponentPropsWithoutRef<"h1" | "h2" | "h3" | "h4" | "h5" | "h6">;
 
 export function HeadingWrapper({
-  className,
-  ...props
+	className,
+	...props
 }: React.ComponentPropsWithoutRef<"div">) {
-  return (
-    <div
-      {...props}
-      className={clsx(
-        className,
-        "mb-6 flex w-full flex-wrap items-end justify-between gap-4 border-b py-2 border-border",
-      )}
-    />
-  );
+	return (
+		<div
+			{...props}
+			className={clsx(
+				className,
+				"mb-6 flex w-full flex-wrap items-end justify-between gap-4 py-2",
+			)}
+		/>
+	);
 }
 
 export function Heading({ className, level = 1, ...props }: HeadingProps) {
-  const Element: `h${typeof level}` = `h${level}`;
+	const Element: `h${typeof level}` = `h${level}`;
 
-  return (
-    <Element
-      {...props}
-      className={clsx(
-        className,
-        "text-3xl/8 font-semibold text-primary sm:text-2xl/8",
-      )}
-    />
-  );
+	return (
+		<Element
+			{...props}
+			className={clsx(
+				className,
+				"text-3xl/8 font-semibold text-primary sm:text-2xl/8",
+			)}
+		/>
+	);
 }
 
 export function Subheading({ className, level = 2, ...props }: HeadingProps) {
-  const Element: `h${typeof level}` = `h${level}`;
+	const Element: `h${typeof level}` = `h${level}`;
 
-  return (
-    <Element
-      {...props}
-      className={clsx(
-        className,
-        "text-base/7 font-semibold text-primary sm:text-sm/6",
-      )}
-    />
-  );
+	return (
+		<Element
+			{...props}
+			className={clsx(
+				className,
+				"text-base/7 font-semibold text-primary sm:text-sm/6",
+			)}
+		/>
+	);
 }
 
 export function Paragraph({
-  className,
-  ...props
+	className,
+	...props
 }: React.ComponentPropsWithoutRef<"p">) {
-  return (
-    <p
-      {...props}
-      className={clsx(className, "text-sm/5 text-muted-foreground")}
-    />
-  );
+	return (
+		<p
+			{...props}
+			className={clsx(className, "text-sm/5 text-muted-foreground")}
+		/>
+	);
 }
