757dd7ffd4b7988f7d5cc13df02e602efbea9360
Matteo La Cognata <matteo.lacognata@gmail.com>
2025-02-09 18:15:26 +0100

    chore(): fix build

--- vite.config.ts
+++ vite.config.ts
@@ -5,38 +5,31 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import dotenvExpand from "dotenv-expand";
 
 export default defineConfig(({ isSsrBuild, command, mode }) => {
-  let define: Record<string, string> = {};
-  if (mode === "development") {
-    const env = loadEnv(mode, process.cwd(), "");
-    dotenvExpand.expand({ parsed: env });
-    define = {
-      "process.env": JSON.stringify(env),
-    };
-  } else {
-    define = {
-      "process.env.BLOB_READ_WRITE_TOKEN": JSON.stringify(
-        process.env.BLOB_READ_WRITE_TOKEN,
-      ),
-      "process.env.SUPABASE_URL": JSON.stringify(process.env.SUPABASE_URL),
-      "process.env.SUPABASE_KEY": JSON.stringify(process.env.SUPABASE_KEY),
-    };
-  }
+	let define: Record<string, string> = {};
+	if (mode === "development") {
+		const env = loadEnv(mode, process.cwd(), "");
+		dotenvExpand.expand({ parsed: env });
+		define = {
+			"process.env": JSON.stringify(env),
+		};
+	} else {
+		define = {
+			"process.env.BLOB_READ_WRITE_TOKEN": JSON.stringify(
+				process.env.BLOB_READ_WRITE_TOKEN,
+			),
+			"process.env.SUPABASE_URL": JSON.stringify(process.env.SUPABASE_URL),
+			"process.env.SUPABASE_KEY": JSON.stringify(process.env.SUPABASE_KEY),
+		};
+	}
 
-  return {
-    define,
-    build: {
-      rollupOptions: isSsrBuild
-        ? {
-            input: "./server/app.ts",
-          }
-        : undefined,
-    },
-    ssr: {
-      noExternal: command === "build" ? true : undefined,
-    },
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-    server: {
-      allowedHosts: true,
-    },
-  };
+	return {
+		define,
+		ssr: {
+			noExternal: command === "build" ? true : undefined,
+		},
+		plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+		server: {
+			allowedHosts: true,
+		},
+	};
 });
