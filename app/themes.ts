fbde6a36ee7d8ace8b9352d0cc42932a234dadb9
Matteo La Cognata <matteo.lacognata@gmail.com>
2025-02-10 10:09:01 +0100

    feat(): add theme switcher

--- app/themes.ts
+++ app/themes.ts
@@ -1,26 +1,43 @@
 import {
-  ThemeVariantEnum,
-  type MuiThemePreset,
-  type StaticThemePreset,
+	ThemeVariantEnum,
+	type MuiThemePreset,
+	type StaticThemePreset,
 } from "@palettebro/theme-generator";
 
+export const themeColors = {
+	blue: {
+		primary: "#3498DB",
+		secondary: "#9595D2",
+		accent: "#E74C3C",
+	},
+	purple: {
+		primary: "#8E44AD",
+		secondary: "#74531C",
+		accent: "#E67E22",
+	},
+	night: {
+		primary: "#34495E",
+		secondary: "#E74C3C",
+		accent: "#ECF0F1",
+	},
+	herbal: {
+		primary: "#16A085",
+		secondary: "#2980B9",
+		accent: "#D35400",
+	},
+};
+
 export const themes = {
-  light: {
-    "color-scheme": "light" as const,
-    variant: ThemeVariantEnum.mui,
-    debug: true,
-    preset: "content" as MuiThemePreset,
-    baseColors: {
-      primary: "#9fc131",
-    },
-  },
-  dark: {
-    "color-scheme": "dark" as const,
-    variant: ThemeVariantEnum.mui,
-    debug: true,
-    preset: "content" as MuiThemePreset,
-    baseColors: {
-      primary: "#9fc131",
-    },
-  },
+	light: {
+		"color-scheme": "light" as const,
+		variant: ThemeVariantEnum.dynamic,
+		debug: true,
+		baseColors: themeColors.blue,
+	},
+	dark: {
+		"color-scheme": "dark" as const,
+		variant: ThemeVariantEnum.dynamic,
+		debug: true,
+		baseColors: themeColors.blue,
+	},
 };
